a11bec2d45febedc046b53a7f4eb685e9c849a8d
xD4rVision <65313449+xD4rVision@users.noreply.github.com>
2020-07-27 21:06:53 +0000

    Update Pars.sh

--- Pars.sh
+++ Pars.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 if [ "$1" == "" ];
 then
-        echo "Analizador de Url"
-        echo "Modo de Uso: ./scriptnet.sh www.desec.com.br"
+        echo "HTML PARSE"
+        echo "USO: ./scriptnet.sh www.exemplo.com.br"
 else
         wget -q $1
         grep href index.html | cut -d "/" -f3 | grep "\." | cut -d '"' -f1 | sort -u | grep -v '<li' > lista
         for url in $(cat lista);do host -T $url | grep "has address" ;done
-        rm lista
-        rm index.html
 fi
 
